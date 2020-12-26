d6e342a15df4f79f828b361c820ea447859b85bb
yarocruz <yarocruz@gmail.com>
2020-12-26 14:44:32 -0500

    finished first project

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -3,9 +3,23 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
+function startLoader() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+function removeLoader() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false
+        loader.hidden = true
+    }
+}
 
 // Get Quote From API
 async function getQuote() {
+    startLoader()
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
     try {
@@ -16,11 +30,31 @@ async function getQuote() {
         } else {
             authorText.innerText = data.quoteAuthor
         }
+        // Reduce size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote')
+        } else {
+            quoteText.classList.remove('long-quote')
+        }
         quoteText.innerText = data.quoteText
+        // Stop Loader show Quote
+        removeLoader()
     } catch (error) {
         console.log('whoops, no quote', error)
     }
 }
 
+function tweetQuote() {
+    const quote = quoteText.innerText
+    const author = authorText.innerText
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+    window.open(twitterUrl, '_blank')
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+
 // On Load
 getQuote()
+

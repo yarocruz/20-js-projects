f190225bee2f3ac29ab07923888a5fdfbcc132e0
yarocruz <yarocruz@gmail.com>
2021-01-13 14:10:50 -0500

    having issue getting event duration on audio component

--- music-player/script.js
+++ music-player/script.js
@@ -1,8 +1,36 @@
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
 const music = document.querySelector('audio')
+const progressContainer = document.getElementById('progress-container')
+const progress = document.getElementById('progress')
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
 
+// Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Metric/Jacinto Design'
+    },
+]
 // Check if playing
 let isPlaying = false
 
@@ -24,3 +52,54 @@ function pauseSong() {
 
 // Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()))
+
+// Update Dom
+function loadSong(song) {
+    title.textContent = song.displayName
+    artist.textContent = song.artist
+    music.src = `music/${song.name}.mp3`
+    image.src = `img/${song.name}.jpg`
+}
+
+// Current Song
+let songIndex = 0
+
+// Previous Song
+function prevSong() {
+    songIndex--
+    if (songIndex < 0) {
+        songIndex = songs.length -  1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+// Next Song
+function nextSong() {
+    songIndex++
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex])
+
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+    console.log(e)
+    if (isPlaying) {
+        const { duration, currentTime } = e.target
+        console.log(duration, currentTime)
+        // Update progress bar
+        const progressPercent = (currentTime / duration ) * 100
+        console.log(progressPercent)
+    }
+}
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+music.addEventListener('timeupdate', updateProgressBar)

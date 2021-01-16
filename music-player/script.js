a3aa7397ed9397f77e89ad611ab37a880e5b00b0
yarocruz <yarocruz@gmail.com>
2021-01-16 15:04:39 -0500

    finished music player project

--- music-player/script.js
+++ music-player/script.js
@@ -92,19 +92,16 @@ loadSong(songs[songIndex])
 // Update Progress Bar & Time
 function updateProgressBar(e) {
     if (isPlaying) {
-        console.log(e)
         const { duration, currentTime } = e.target
         // Update progress bar
         const progressPercent = (currentTime / duration ) * 100
        progress.style.width = `${progressPercent}%`
         // Calculate display for duration
         const durationMinutes = Math.floor(duration / 60)
-        console.log('minutes', durationMinutes)
         let durationSeconds = Math.floor(duration % 60)
         if (durationSeconds < 10) {
             durationSeconds = `0${durationSeconds}`
         }
-        console.log('seconds', durationSeconds)
 
        // Delay switching duration Element to avoid NaN
        if (durationSeconds) {
@@ -112,17 +109,25 @@ function updateProgressBar(e) {
        }
         // Calculate display for current time
         const currentMinutes = Math.floor( parseInt(currentTime) / 60)
-        console.log('minutes', currentMinutes)
         let currentSeconds = Math.floor( parseInt(currentTime) % 60)
         if (currentSeconds < 10) {
             currentSeconds = `0${currentSeconds}`
         }
-        console.log('seconds', currentSeconds)
         currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`
     }
+}
+
+// Set Progress Bar
+function setProgressBar(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const { duration } = music
+    music.currentTime = (clickX / width) * duration
 }
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+music.addEventListener('ended', nextSong)
 music.addEventListener('timeupdate', updateProgressBar)
+progressContainer.addEventListener('click', setProgressBar)

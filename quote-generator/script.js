9e6328751cf3ae43ba2a802b820e0ec922fac012
yarocruz <yarocruz@gmail.com>
2021-02-14 20:42:42 -0500

    there might be an issue with the forismatic apit

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -21,7 +21,7 @@ function removeLoader() {
 async function getQuote() {
     startLoader()
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en'
     try {
         const response = await fetch(proxyUrl + apiUrl)
         const data = await response.json()
